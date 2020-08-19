1b85d71d4f36a1231b043848277e9244edb00b6c
Theelipan Prabakar <theelipan@gmail.com>
2020-08-18 23:13:30 -0400

    catch error

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const search = document.getElementById("searchField");
 const display = document.getElementById("displayDiv");
 
 const getRecipe = async (searchRecipe) => {
-  let res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchRecipe}`);
-  let data = await res.json();
-
-  display.innerHTML = `
-      <div>
-      <h1 class="display-5 text-white">${data.meals[0].strMeal}</h1>
-      <h4 class="text-white pt-1">${data.meals[0].strArea}  |  ${data.meals[0].strCategory} </h4>
-      </div>`;
+  try {
+    let res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchRecipe}`);
+    let data = await res.json();
+    display.innerHTML = `
+    <div>
+    <h1 class="display-5 text-white">${data.meals[0].strMeal}</h1>
+    <h4 class="text-white pt-1">${data.meals[0].strArea}  |  ${data.meals[0].strCategory} </h4>
+    </div>`;
+  } catch (err) {
+    // catches errors both in fetch and response.json
+    display.innerHTML = `<h1 class="display-5 text-white">Search Not Found</h1>`;
+  }
 };
 
 //   <img src="${data.meals[0].strMealThumb}" class="img-fluid" alt="Responsive image" width="200px">
-search.addEventListener("input", () => {
-  if (getRecipe(search.value) !== "") {
-    getRecipe(search.value);
-  }
-});
+search.addEventListener("input", () => getRecipe(search.value));
