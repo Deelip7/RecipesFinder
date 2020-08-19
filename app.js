b60d94e906fca08e096a1ebd956cecf45a8d7acc
Theelipan Prabakar <theelipan@gmail.com>
2020-08-19 15:01:33 -0400

    autocomplete user input and add it it UI

--- app.js
+++ app.js
@@ -3,30 +3,38 @@ const search = document.getElementById("searchField");
 const display = document.getElementById("displayDiv");
 
 const getRecipe = async (searchRecipe) => {
-  const alertMsg = `<h1 class="display-5 text-white">Search Not Found</h1>`;
+  const alertMsg = `<h1 class="display-5 text-white p-2">Search Not Found</h1>`;
   try {
     let res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchRecipe}`);
     let data = await res.json();
-    display.innerHTML = `
-    <div>
-    <h1 class="display-5 text-white">${data.meals[0].strMeal}</h1>
-    <h4 class="text-white pt-1">${data.meals[0].strArea}  |  ${data.meals[0].strCategory} </h4>
-    </div>`;
+    let arry = Object.values(data.meals);
+
+    let findMatch = arry.filter((el) => {
+      return el;
+    });
+
+    display.innerHTML = "";
+    findMatch.forEach((element) => {
+      display.innerHTML += `<button id="${element.idMeal}"type="submit" class="display-5 p-3 m-2 bg-white rounded-pill h4">${element.strMeal}</button>`;
+    });
   } catch (err) {
     // catches errors both in fetch and response.json
     display.innerHTML = alertMsg;
+    console.log(err);
   }
-  if (searchRecipe.length === 0) {
-    display.innerHTML = alertMsg;
-  }
+
+  searchRecipe.length === 0 ? (display.innerHTML = "") : showMeal();
 };
 
 search.addEventListener("input", () => getRecipe(search.value));
 
 //   <img src="${data.meals[0].strMealThumb}" class="img-fluid" alt="Responsive image" width="200px">
 
-//   let findMatch = data.filter((e) => {
-//     let userValue = new RegExp(`^${searchRecipe}`, "gi");
-//     return data.meals[0].strMeal.match(userValue);
-//   });
-//   console.log(findMatch);
+function showMeal() {
+  let buttonList = document.querySelectorAll("button");
+  buttonList.forEach((btnElement) => {
+    btnElement.addEventListener("click", (el) => {
+      console.log(el.target.id);
+    });
+  });
+}
