d6afa4344a5162b85d2f80dafef7088cee14b6fa
Theelipan Prabakar <theelipan@gmail.com>
2020-08-19 21:52:41 -0400

    update UI

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const displayResult = document.getElementById("displayResult");
 
 const getRecipe = async (searchRecipe) => {
   let mealArray = [];
-  const alertMsg = `<h1 class="display-5 text-white p-2 border border-danger">Search Not Found</h1>`;
+  const alertMsg = `<img src="Images/alert-circle.svg" width="50px" class="border-0 mb-3"/>
+                  <h1 class="display-5 text-white p-3 border border-danger h3">No Result Found</h1>`;
   try {
     let res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchRecipe}`);
     let data = await res.json();
@@ -17,7 +18,7 @@ const getRecipe = async (searchRecipe) => {
 
     displayAutoComplete.innerHTML = "";
     findMatch.forEach((element) => {
-      displayAutoComplete.innerHTML += `<button id="${element.idMeal}"type="submit" class="display-5 p-3 m-2 bg-white rounded-pill h4">${element.strMeal}</button>`;
+      displayAutoComplete.innerHTML += `<button id="${element.idMeal}"type="submit" class="display-5 p-3 m-2 bg-white rounded-pill h5">${element.strMeal}</button>`;
     });
   } catch (err) {
     // catches errors both in fetch and response.json
@@ -50,41 +51,45 @@ function UI(mealUI) {
   let filteredIngredient = filtered_keys(mealUI, /strIngredient/);
   let filteredMeasure = filtered_keys(mealUI, /strMeasure/);
 
-  console.log(filteredIngredient);
-  console.log(filteredMeasure);
+  //--- clear input field
   search.value = "";
-  displayResult.innerHTML = `<img src="${mealUI.strMealThumb}" class="img-fluid m-4 rounded-pill" alt="Responsive image" width="200px">
-                             <h1 class="text-white mb-3"> ${mealUI.strMeal}</h1> 
-                             <h4 class="text-white mb-5 mx-auto"> 
-                             <img src="Images/map-pin.svg"  width="25px"/>
+  //--- Add table to UI
+  displayResult.innerHTML = `
+                            <img src="${mealUI.strMealThumb}" class="img-fluid m-4 rounded-pill" alt="Responsive image" width="200px">
+                            <h1 class="text-white mb-3"> ${mealUI.strMeal}</h1> 
+                             
+                            <h4 class="text-white mb-5 mx-auto"> 
+                            <img src="Images/map-pin.svg"  width="25px"/>
                              ${mealUI.strArea}<span class="h2 text-white"> | </span>
-                             <img src="Images/grid.svg" width="25px">
+                            <img src="Images/grid.svg" width="25px">
                              ${mealUI.strCategory} 
-                             </h4> 
-                             <p class="h4 text-white mb-3 "> 
+                            </h4> 
+
+                            <p class="h4 text-white mb-3" id="instruction"> 
                               <span class="h3 text-white">Instructions: </span> 
                               ${mealUI.strInstructions.replace(/\./g, ".<br>")} 
-                             </p>
-                            
-                            <table class="table  mx-auto w-100 text-center ">                            
-                            <thead>
-                            <tr class="text-center"> 
-                               <th class="pb-1" scope="col">Ingredient</th>
-                               <th class="pb-1" scope="col" >Amount</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr >
-                              <td class="text-center px-0">${filteredIngredient}</td>
-                              <td class="text-center px-0">${filteredMeasure}</td>
-                            </tr>
-                            </tbody> 
+                            </p>
+                          
+                            <table class="table  mx-auto w-100 text-center">                            
+                              <thead>
+                                <tr class="text-center"> 
+                                  <th class="pb-1" scope="col">Ingredient</th>
+                                  <th class="pb-1" scope="col" >Amount</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td class="text-center px-0">${filteredIngredient}</td>
+                                  <td class="text-center px-0">${filteredMeasure}</td>
+                                </tr>
+                              </tbody> 
                             </table>
-                            <img src="Images/link-2.svg"  width="30px"/>
-                            <a href="${mealUI.strSource}" class="text-white mb-3 h3">Source</a> 
-                            <img src="Images/youtube.svg"  width="30px"/>
-                            <a href="${mealUI.strYoutube}" class="text-white mb-3 h3">Youtube Link</a> 
-
+                            <div class="source">
+                              <img src="Images/link-2.svg"  width="25px" class="border-0 mb-2 mr-2"/>
+                                <a href="${mealUI.strSource}" target="_blank" class="text-white h4 mr-4"> Source</a> 
+                              <img src="Images/youtube.svg"  width="25px" class="border-0 mb-2 mr-2"/>
+                                <a href="${mealUI.strYoutube}" target="_blank" class="text-white h4 mr-2"> Youtube</a> 
+                            </div>
                             `;
 }
 
@@ -100,5 +105,3 @@ let filtered_keys = (obj, filter) => {
     }
   return keys.join("");
 };
-
-// <span class="badge badge-primary badge-pill">14</span>
