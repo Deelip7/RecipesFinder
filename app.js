ea76070839a1404c4bbb0c73fed3bb92550349c9
Theelipan Prabakar <theelipan@gmail.com>
2020-08-21 19:02:19 -0400

    add model for saved recipe

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Global Variables for DOM manipulation
 const btn = document.getElementById("submitBtn");
 const searchField = document.getElementById("searchField");
 const displayAutoComplete = document.getElementById("displaySuggestions");
 const displayResult = document.getElementById("displayResult");
+const saveFavoritesDiv = document.getElementById("saveFavoritesDiv");
 
 /*
 /------------------------------------------------------------
@@ -22,7 +24,6 @@ class Favorite {
 /------------------------------------------------------------
 /   Set/Get/Remove date from LocalStorage.
 */
-
 class StoreData {
   static getData() {
     let plans;
@@ -32,12 +33,17 @@ class StoreData {
     } else {
       plans = JSON.parse(localStorage.getItem("plans")); // if "plans" array exists return it to addData()
     }
-
     return plans;
   }
 
   static addData(plan) {
-    document.getElementById("saveBtn").addEventListener("click", (e) => {
+    const saveFavoritesBtn = document.getElementById("saveBtn");
+    saveFavoritesBtn.addEventListener("click", (e) => {
+      saveFavoritesBtn.innerHTML = ` <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="red" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                    </svg> 
+                                    `;
+      saveFavoritesDiv.classList.toggle("show");
       let hasId = false;
       const plans = StoreData.getData(); //array from local storage
 
@@ -65,7 +71,6 @@ class StoreData {
     localStorage.setItem("plans", JSON.stringify(plans)); //push new data from user to local storage
   }
 }
-
 /*
 /------------------------------------------------------------
 /  UI Classes
@@ -73,7 +78,6 @@ class StoreData {
 / Update UI dynamical as user types. Display Recipe based on button click
 / Update DOM with API data and store to localStorage if needed
 */
-
 class UI {
   // Get Recipe using API data
   static getRecipe = async (searchRecipe) => {
@@ -98,10 +102,11 @@ class UI {
   // Alert user if no match is found
   static noRecipeFound() {
     const alertMsg = `<img src="Images/alert-circle.svg" width="50px" class="border-0 mb-3"/>
-    <h1 class="display-5 text-white p-3 border border-danger h3">No Result Found</h1>`;
+                      <h1 class="display-5 text-white p-3 border border-danger h3">No Result Found</h1>`;
     displayAutoComplete.innerHTML = alertMsg;
   }
 
+  // Autocomplete users search and display to UI
   static showRecipeOptions(mealList) {
     document.querySelectorAll("button").forEach((btnElement) => {
       btnElement.addEventListener("click", (el) => {
@@ -141,8 +146,13 @@ class UI {
     // Add API data to UI
     displayResult.innerHTML = `
                               <img src="${mealUI.strMealThumb}" class="img-fluid mt-4 rounded-pill" alt="Responsive image" width="200px">
-                              <button type="button" class="btn btn-outline-light m-4 rounded-pill" id="saveBtn">Save &#x2764 Recipe</button>
-  
+
+                              <button type="button" class="btn btn-outline-light m-4 rounded-pill" id="saveBtn"> 
+                                <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                </svg>
+                              </button>
+
                               <h1 class="text-white mb-3"> ${mealUI.strMeal}</h1> 
                                
                               <h4 class="text-white mb-5 mx-auto"> 
@@ -187,7 +197,7 @@ class UI {
 /------------------------------------------------------------
 /   Events
 /------------------------------------------------------------
-/   Pass searchField value to UI class
+/   Pass Input Field value to UI class
 /
 */
 searchField.addEventListener("input", () => {
